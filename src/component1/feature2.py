34088a2aef50addc13b6cdb5f60b2f7124936b79
bayobiyi <adebayoadebiyi@gmail.com>
2020-04-10 05:58:33 +0000

    Add feature2

--- src/component1/feature2.py
+++ src/component1/feature2.py
@@ -1,2 +1,3 @@
 print(" Programer_3_date_4: created pseudocode for feature 2")
 print(" Programer_3_date_15 : completed implementing first half of feature 2")
+print(" Programer_3_date_20 : completed implementing second half of feature 2")
