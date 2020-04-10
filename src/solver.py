58de1018613bb107b93ba6f918a7dc4f808cadb3
bayobiyi <adebayoadebiyi@gmail.com>
2020-04-10 08:42:20 +0000

    Add version 2.0

--- src/solver.py
+++ src/solver.py
@@ -1,2 +1,3 @@
 print(" Programer_1_date_1: solver created to solve a simple case")
-print(" Programer_1_date_1: version 1.0 of solver")
+print(" Programer_1_date_1:  version 1.0 of solver")
+print(" Programer_1_date_50: version 2.0 of solver")
