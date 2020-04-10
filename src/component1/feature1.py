1f942b9e6bf9adfdcb8e4526b77f9e7df892c4c2
bayobiyi <adebayoadebiyi@gmail.com>
2020-04-10 05:36:26 +0000

    Put debug print statments

--- src/component1/feature1.py
+++ src/component1/feature1.py
@@ -1,1 +1,2 @@
-print(" Programer_1_date_1: : implemented feature 1")
+print(" Programer_1_date_1: implemented feature 1")
+print(" Programer_2_date_2: started working on issue1 (fix version 1.0 bug)")
