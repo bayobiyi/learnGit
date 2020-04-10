958835c8123edbb72f0415b44495890b6a7ec64f
bayobiyi <adebayoadebiyi@gmail.com>
2020-04-10 06:24:10 +0000

    Add that feature 1 is related to component 1

--- src/component1/feature1.py
+++ src/component1/feature1.py
@@ -1,4 +1,4 @@
-print(" Programer_1_date_1: implemented feature 1")
+print(" Programer_1_date_1: implemented feature 1 related to component1")
 print(" Programer_2_date_2: started working on issue1 (fix version 1.0 bug)")
 print(" Programer_2_date_5: fixed the first bug")
 print(" Programer_2_date_20: fixed the last bug")
