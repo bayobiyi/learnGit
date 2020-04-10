6bfa0ad69c12441b03b0c3967792ee4fea8c0482
bayobiyi <adebayoadebiyi@gmail.com>
2020-04-10 05:52:01 +0000

    Add first half of feature 2

--- src/component1/feature2.py
+++ src/component1/feature2.py
@@ -1,1 +1,2 @@
-print(" Programer_3_date_4: : created pseudocode for feature 2")
+print(" Programer_3_date_4: created pseudocode for feature 2")
+print(" Programer_3_date_15 : completed implementing first half of feature 2")
