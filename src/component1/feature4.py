2455f959c32c70553a9c40cc0c6506128b68f800
bayobiyi <adebayoadebiyi@gmail.com>
2020-04-27 12:54:56 +0000

    Implement the actual functionality

--- src/component1/feature4.py
+++ src/component1/feature4.py
@@ -1,2 +1,3 @@
 print(" Programer_1_date_60: created pseudocode for feature 4")
 print(" Programer_1_date_63: implemented workflow for feature 4")
+print(" Programer_1_date_65: implemented the actual functionality")
