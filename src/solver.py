5a6dc4a5ec361f2a28537bf6e76e2c737c5c9ed8
bayobiyi <adebayoadebiyi@gmail.com>
2020-04-27 12:28:42 +0000

    Try to load foo.py into solver.py

--- src/solver.py
+++ src/solver.py
@@ -1,3 +1,14 @@
+from importlib import util
+
+def load_file(name, path):
+    spec = util.spec_from_file_location(name, path)
+    module = util.module_from_spec(spec)
+    print(module)
+    spec.loader.exec_module(module)
+    return module
+  
 print(" Programer_1_date_1: solver created to solve a simple case")
 print(" Programer_1_date_1:  version 1.0 of solver")
 print(" Programer_1_date_50: version 2.0 of solver")
+
+feature1 = load_file('foo', './foo.py')
